5020367ea08eae0b84d432c253bb4f72a321ef0c
MarcW88 <146358493+MarcW88@users.noreply.github.com>
2023-10-02 08:26:18 +0200

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -6,13 +6,13 @@
         $message = trim($_POST["message"]);
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            echo "Oups ! Il semble y avor eu un problème. Veuillez compléter le formulaire et réessayer.";
             exit;
         }
         /***********************************/
         /*     Update Your Mail Below      */
         /***********************************/
-        $recipient = "yourmail@example.com";
+        $recipient = "marc.williame@hotmail.com";
 
         $subject = "New contact from $name";
         $email_content = "Name: $name\n";
@@ -21,13 +21,13 @@
         $email_headers = "From: $name <$email>";
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Merci ! Votre message a été envoyé";
         } else {
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oups ! Un problème s'est produit et votre message n'a pu être envoyé.";
         }
     } else {
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Il y a eu un problème avec votre message, veuillez réessayer.";
     }
 ?>
